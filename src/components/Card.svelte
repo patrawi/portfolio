efef5a7130ec3e8dca2aafb0d0dceacc88561f1a
= <=>
2024-10-09 13:26:16 +0700

    feat: experience timeline

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,13 +1,42 @@
-<script></script>
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+	import type { IWorkExperience } from '$lib/types';
+	export let job;
+	const { company, duration, position, responsibilities } = job as IWorkExperience;
+</script>
 
 <div
-	class="absolute left-0 top-0 bg-black h-full *:ml-auto p-4 rounded-lg text-left w-full overflow-auto"
+	transition:slide={{ duration: 500 }}
+	class="absolute left-0 top-0 bg-gray-800/85 backdrop-blur-xl h-full *:ml-auto p-4 rounded-lg text-left w-full overflow-y-auto overflow-x-hidden scrollbar-thin"
 >
-	<p class="">
-		- Created a script for scraping data from intranet website using Puppeteer and Line Notify APIs
-		to notify tasks for team. - Modified login pages for several websites including both internal
-		and external uses by using ThaID APIs and PHP improving user experience in logging in. -
-		Developed a website application for managing employees in Auction Announcement Printing company
-		using Laravel and VueJS.
-	</p>
+	<div class="space-y-2">
+		<div id="position">
+			<h2 class="text-xl font-bold">{position}</h2>
+			<p class="text-orange-400">@{company}</p>
+		</div>
+		<div class="flex items-center gap-2">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="size-6"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
+				/>
+			</svg>
+			<p>{duration}</p>
+		</div>
+		<ul class="list-disc px-4 space-y-1 text-sm break-words">
+			{#each responsibilities as responsibilitie}
+				<li>
+					{responsibilitie}
+				</li>
+			{/each}
+		</ul>
+	</div>
 </div>
--- src/components/Timeline.svelte
+++ src/components/Timeline.svelte
@@ -1,84 +1,88 @@
-<script>
+<script lang="ts">
 	import { tilt } from '$lib/tilt';
-	import motorhub from '$lib/assets/motorhub_icon.jpg';
+	import motor from '../lib/assets/led_logo.png';
 	import Card from './Card.svelte';
-	let hovering = false;
-	const handleMouseEnter = () => {
-		hovering = true;
+	import experienceData from '$lib/data/experience.json';
+	const { workExperience } = experienceData;
+
+	let hoverStates: boolean[] = workExperience.map(() => false);
+
+	const images = import.meta.glob('../lib/assets/*.{png,jpg,jpeg,svg}', { eager: true });
+	const getImgUrl = (imageFileName: string) => {
+		const imagePath = `../lib/assets/${imageFileName}`;
+		if (images[imagePath]) {
+			return (images[imagePath] as { default: string }).default;
+		}
+		return 'src/libs/assets/test.webp';
 	};
-	const handleMouseLeave = () => {
-		hovering = false;
+	const handleMouseEnter = (index: number) => {
+		hoverStates[index] = true;
+
+		hoverStates = [...hoverStates];
+	};
+	const handleMouseLeave = (index: number) => {
+		hoverStates[index] = false;
+		hoverStates = [...hoverStates];
 	};
 </script>
 
 <div
 	id="timeline"
-	class="relative space-y-8 before:absolute before:bg-black before:w-0.5 rounded-lg before:h-full before:left-1/2 before:right-0 before:bottom-0 before:top-0 text-white"
+	class="relative space-y-8 before:absolute before:bg-black before:w-0.5 rounded-lg
+	before:h-full before:left-1/2 before:right-0 before:bottom-0 before:top-0 text-white"
 >
-	<div class="flex justify-center items-center w-full cursor-pointer" id="checkpoint">
-		<div
-			class=" w-1/2 text-right pr-8"
-			role="contentinfo"
-			use:tilt={{ scale: 1.05, factor: 0.01 }}
-			on:mouseenter={handleMouseEnter}
-			on:mouseleave={handleMouseLeave}
-		>
+	{#each workExperience as job, i}
+		<div class="flex justify-center items-center w-full cursor-pointer" id="checkpoint">
 			<div
-				class="relative bg-[#212A3C]/50 max-w-xl h-[300px] ml-auto p-4 rounded-lg text-left border-black border-2 shadow-xl flex justify-center items-center gap-4"
+				class={` w-1/2   ${i % 2 === 0 ? 'text-right pr-8' : ''}`}
+				use:tilt={{ scale: 1.05, factor: 0.01 }}
+				role="contentinfo"
+				on:mouseenter={() => handleMouseEnter(i)}
+				on:mouseleave={() => handleMouseLeave(i)}
 			>
-				<img src={motorhub} alt="An alt text" class="w-36 h-36 rounded-xl shadow-xl" />
-				<div class="">
-					<div>
-						<h2 class="text-xl font-bold">Fullstack Developer</h2>
-						<div>tag</div>
+				{#if i % 2 == 0}
+					<div
+						class="relative bg-[#212A3C]/50 max-w-xl h-[300px] ml-auto p-4 rounded-lg text-right border-black border-2 shadow-xl flex justify-center items-center gap-4"
+					>
+						<img
+							src={getImgUrl(job.image)}
+							alt="An alt text"
+							class="w-36 h-36 rounded-xl shadow-xl"
+						/>
+
+						{#if hoverStates[i]}
+							<Card {job} />
+						{/if}
 					</div>
-					<div class="flex items-center gap-2">
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							class="size-6"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5"
-							/>
-						</svg>
-						<p>October 2024 - Present</p>
+				{/if}
+			</div>
+
+			<div
+				class={`absolute rounded-full ml-0.5 w-5 bg-blue-100 h-5 ${i === 0 ? 'top-0' : ''}`}
+			></div>
+			<div
+				class={` w-1/2   ${i % 2 !== 0 ? 'text-left pl-8' : ''}`}
+				role="contentinfo"
+				use:tilt={{ scale: 1.05, factor: 0.01 }}
+				on:mouseenter={() => handleMouseEnter(i)}
+				on:mouseleave={() => handleMouseLeave(i)}
+			>
+				{#if i % 2 !== 0}
+					<div
+						class="relative bg-[#212A3C]/50 max-w-xl h-[300px] ml-auto p-4 rounded-lg text-left border-black border-2 shadow-xl flex justify-center items-center gap-4"
+					>
+						<img
+							src={getImgUrl(job.image)}
+							alt="An alt text"
+							class="w-36 h-36 rounded-xl shadow-xl"
+						/>
+
+						{#if hoverStates[i]}
+							<Card {job} />
+						{/if}
 					</div>
-				</div>
-				{#if hovering}
-					<Card />
 				{/if}
 			</div>
 		</div>
-
-		<div class="absolute top-0 rounded-full ml-0.5 w-5 bg-blue-100 h-5"></div>
-		<div class="w-1/2" />
-	</div>
-
-	<div class="flex justify-center items-center w-full">
-		<div class="w-1/2"></div>
-		<div class="absolute rounded-full w-5 bg-blue-100 h-5"></div>
-		<div id="checkpoint" class="w-1/2 text-left pl-8">
-			<div class="">
-				<h2>Day 1: Helsinki, Finland</h2>
-				<p>Lorem ipsum dolor sit amet</p>
-			</div>
-		</div>
-	</div>
-	<div class="flex justify-center items-center w-full">
-		<div id="checkpoint" class=" w-1/2 text-right pr-8">
-			<div class="">
-				<h2>Day 1: Helsinki, Finland</h2>
-				<p>Lorem ipsum dolor sit amet</p>
-			</div>
-		</div>
-
-		<div class="absolute rounded-full ml-0.5 w-5 bg-blue-100 h-5"></div>
-		<div class="w-1/2" />
-	</div>
+	{/each}
 </div>
